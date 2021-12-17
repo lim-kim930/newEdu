6f0ba990b0b503138b62c4fe888e119a5cf4f085
limkim930 <1625753207lim@gmail.com>
2021-12-17 22:46:02 +0800

    new

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (location.search.split("?")[1] !== undefined) {
         redirect: "follow",
         headers: { "Authorization": "token " + location.search.split("?")[1].split("&")[0].split("=")[1] }
     };
-    fetch("https://api.hduhelp.com/base/person/info", requestOptions)
+    return fetch("https://api.hduhelp.com/base/person/info", requestOptions)
         .then(response => response.text())
         .then(result => {
             if (JSON.parse(result).data.STAFFTYPE === "1") {
@@ -32,13 +32,9 @@ if (location.search.split("?")[1] !== undefined) {
             alert(error);
         });
 }
-else if (localStorage.getItem("jw_student_file") === null && localStorage.getItem("jw_ent_file") === null)
-    window.location.href = "https://edu.limkim.cn/sign";
-else if (localStorage.getItem("jw_student_file") !== null && localStorage.getItem("jw_ent_file") !== null)
-    window.location.href = "https://edu.limkim.cn/student";
-
-else if (localStorage.getItem("jw_student_file") !== null && localStorage.getItem("jw_ent_file") === null)
-    window.location.href = "https://edu.limkim.cn/student";
-
-else if (localStorage.getItem("jw_student_file") === null && localStorage.getItem("jw_ent_file") !== null)
-    window.location.href = "https://edu.limkim.cn/company";
+if (localStorage.getItem("jw_student_file") !== null)
+    return window.location.href = "https://edu.limkim.cn/student";
+if (localStorage.getItem("jw_ent_file") !== null)
+    return window.location.href = "https://edu.limkim.cn/company";
+if (localStorage.getItem("jw_manager_file") !== null)
+    return window.location.href = "https://edu.limkim.cn/manager";
